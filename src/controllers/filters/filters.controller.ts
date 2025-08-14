c05f685de638c9f4c5acab954d83ee7e6d275d40
Rodrigo Guardia <rodrigo.guardia316@gmail.com>
2025-08-14 19:54:54 -0400

    Feat: saving user filters on table

--- src/controllers/filters/filters.controller.ts
+++ src/controllers/filters/filters.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { BasicFiltersDTO } from 'src/DTO/basicFilters.dto';
+import { FiltersService } from 'src/services/filters/filters.service';
 
 @Controller('filters')
-export class FiltersController {}
+export class FiltersController {
+  constructor(private readonly filterService: FiltersService) {}
+
+  @Post('save')
+  @UseGuards(AuthGuard('jwt'))
+  async saveFilterStatus(@Body() filters: BasicFiltersDTO, @Request() req) {
+    const userId = req.user.id;
+    return await this.filterService.saveFilters(filters, userId);
+  }
+}
