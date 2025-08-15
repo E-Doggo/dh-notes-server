290950f62af18b35f94896784544e84c253452f3
Rodrigo Guardia <rodrigo.guardia316@gmail.com>
2025-08-14 20:01:31 -0400

    Feat: fetch user filter

--- src/controllers/filters/filters.controller.ts
+++ src/controllers/filters/filters.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { BasicFiltersDTO } from 'src/DTO/basicFilters.dto';
 import { FiltersService } from 'src/services/filters/filters.service';
@@ -6,6 +13,13 @@ import { FiltersService } from 'src/services/filters/filters.service';
 @Controller('filters')
 export class FiltersController {
   constructor(private readonly filterService: FiltersService) {}
+
+  @Get('')
+  @UseGuards(AuthGuard('jwt'))
+  async getUserFilter(@Request() req) {
+    const userId = req.user.id;
+    return await this.filterService.findFilterByUser(userId);
+  }
 
   @Post('save')
   @UseGuards(AuthGuard('jwt'))
